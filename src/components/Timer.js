1e397c3865052f42416ec77eec8ff8947530e944
chutting <tingting.chu@thoughtworks.com>
2020-09-23 23:07:18 +0800

    [Chu] feat: complete the style and layout of the timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 
 class Timer extends Component {
   render() {
-    return <p>timer</p>
+    return <div className="timer-page">
+      <h1>在线倒计时器</h1>
+      <div className="timer-content">
+        <div className="timer-start">
+          <div className="timer-set">
+            <p>设置事件</p>
+            <input type="text" className="timer-input"></input>
+          </div>
+          <input type="submit" value="Start" className="timer-start-btn"/>
+        </div>
+        <div className="timer-show">
+          <p>事件</p>
+        </div>
+      </div>
+      <Link to="/" id="return-home">回到主页</Link>
+    </div>
   }
 
 }
