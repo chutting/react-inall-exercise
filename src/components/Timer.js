311cc13bcb5fc01ad00bd82ff19ea629d3534385
chutting <tingting.chu@thoughtworks.com>
2020-09-24 20:00:31 +0800

    [Chu] refactor: refact timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -14,24 +14,27 @@ class Timer extends Component {
         status: 'End'
       })
     } else {
-      this.setState({
-        status: `${this.state.currentTime} Seconds`
-      });
-
-      let timer = setInterval(() => {
-        this.setState({
-          currentTime: this.state.currentTime - 1,
-          status: `${this.state.currentTime - 1} Seconds`
-        })
-  
-        if (this.state.currentTime === 0) {
-          clearInterval(timer);
-          this.setState({
-            status: 'End'
-          })
-        }
-      }, 1000);
+      this.timerInvoke()
     }
+  }
+
+  timerInvoke = () => {
+    this.setState({
+      status: `${this.state.currentTime} Seconds`
+    });
+    let timer = setInterval(() => {
+      this.setState({
+        currentTime: this.state.currentTime - 1,
+        status: `${this.state.currentTime - 1} Seconds`
+      })
+
+      if (this.state.currentTime === 0) {
+        clearInterval(timer);
+        this.setState({
+          status: 'End'
+        })
+      }
+    }, 1000);
   }
 
   handleInit = (event) => {
